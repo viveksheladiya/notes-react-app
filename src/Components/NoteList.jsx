997a73c44cd069e9ecc4e05fbb2dae24824ff0df
Vivek Sheladiya <vivekheladiya32@gmail.com>
2023-05-22 10:40:35 +0530

    Updated

---
+++ src/Components/NoteList.jsx
@@ -0,0 +1,24 @@
+import Note from './Note';
+import AddNotes from './AddNotes';
+
+const NoteList = ({
+	notes,
+	handleAddNote,
+	handleDeleteNote,
+}) => {
+	return (
+		<div className='notes-list'>
+			{notes.map((note) => (
+				<Note
+					id={note.id}
+					text={note.text}
+					date={note.date}
+					handleDeleteNote={handleDeleteNote}
+				/>
+			))}
+			<AddNotes handleAddNote={handleAddNote} />
+		</div>
+	);
+};
+
+export default NoteList;
